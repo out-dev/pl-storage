d9d4334b53a46e3f023478e1a72dd67cab73e29b
out-dev <s.r.schmitt@web.de>
2021-10-23 13:59:54 -0400

    playaround with table storage

--- go.mod
+++ go.mod
@@ -11,5 +11,6 @@ require (
 	github.com/stretchr/testify v1.7.0 // indirect
 	golang.org/x/net v0.0.0-20210610132358-84b48f89b13b // indirect
 	golang.org/x/text v0.3.6 // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
--- main.go
+++ main.go
@@ -1,18 +1,36 @@
 package main
 
 import (
+	SC "pl-storage/storage_configuration"
+
+	"context"
 	"fmt"
 
 	"github.com/Azure/azure-sdk-for-go/sdk/data/aztables"
 )
 
 func main() {
-	cred, err := aztables.NewSharedKeyCredential("myAccountName", "myAccountKey")
+	var options *SC.StorageConfiguration = &SC.StorageConfiguration{}
+	options.GetDefault()
+	var azCfg, err = options.ToAzureTableClientOptions()
+	serviceClient, err := aztables.NewServiceClientFromConnectionString("", &azCfg)
 	if(err != nil){
 		fmt.Print(err.Error())
 	}
-	serviceClient, err := aztables.NewServiceClient("https://<myAccountName>.table.core.windows.net/", cred, nil)
-	if(err != nil){
-		fmt.Print(err.Error())
+	tableOptions := aztables.CreateTableOptions{}
+	serviceClient.CreateTable(context.Background(), "testtable", &tableOptions)
+	
+	var data map[string]interface{} = map[string]interface{}{
+		"data": "simon",
 	}
+
+	myEntity := aztables.EDMEntity{
+		Entity:     aztables.Entity{PartitionKey: "001234", RowKey: "RedMarker"},
+		Properties: data,
+	}
+
+	marshalled, err := myEntity.MarshalJSON()
+	
+	client := serviceClient.NewClient("testtable")
+	client.AddEntity(context.Background(), marshalled, nil)
 }
